b1623761d2bfac79c7f41fc47d12ccaed271e2f3
william-wxm <602567571@qq.com>
2020-02-02 20:48:37 +0800

    加入登录状态

--- src/main/java/life/weike/community/community/controller/AuthorizeContorller.java
+++ src/main/java/life/weike/community/community/controller/AuthorizeContorller.java
@@ -5,9 +5,12 @@ import life.weike.community.community.dto.AccessTokenDTO;
 import life.weike.community.community.dto.GithubUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeContorller {
@@ -23,7 +26,8 @@ public class AuthorizeContorller {
     private githubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code ,
-                           @RequestParam (name="state")String state){
+                           @RequestParam (name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -32,8 +36,11 @@ public class AuthorizeContorller {
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.getName());
-
-        return "index";
+        if (githubUser != null){
+            request.getSession().setAttribute("githubUser",githubUser);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
     }
 }
