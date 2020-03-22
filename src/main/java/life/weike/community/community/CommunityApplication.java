b58312a56b184c8a570abf21d623efed5484f4c0
william-wxm <602567571@qq.com>
2020-03-22 12:57:05 +0800

    tijiao

--- src/main/java/life/weike/community/community/CommunityApplication.java
+++ src/main/java/life/weike/community/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package life.weike.community.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "life.weike.community.community.mapper")
 public class CommunityApplication {
 
     public static void main(String[] args) {
